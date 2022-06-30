f75e78bd6cdb5e409b10c502444fc1ccafb24583
rafael.cagliari <rafael.cagliari22@gmail.com>
2022-06-29 22:57:37 -0300

    implemented navigation with voyager

--- app/src/main/java/com/example/pokedex_compose/MainActivity.kt
+++ app/src/main/java/com/example/pokedex_compose/MainActivity.kt
@@ -23,6 +23,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.example.pokedex_compose.model.Pokemon
 import com.example.pokedex_compose.ui.theme.GrassTypeGreen
 import com.example.pokedex_compose.ui.theme.Pokedex_composeTheme
@@ -31,76 +35,82 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Pokedex_composeTheme {
-                PokemonList(pokemonList = Pokemon.pokemonList)
-            }
+            Navigator(screen = RenderPokemonList(Pokemon.pokemonList))
         }
     }
 
-    @Composable
-    fun PokemonCard(pokemon: Pokemon) {
-        Card(
-            modifier = Modifier
-                .padding(5.dp)
-                .fillMaxWidth()
-                .height(200.dp)
-                .clickable {  }, shape = MaterialTheme.shapes.medium,
-            backgroundColor = pokemon.color
-        ) {
-            Column(
-                Modifier.padding(horizontal = 10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    modifier = Modifier.size(100.dp),
-                    painter = painterResource(id = pokemon.image), contentDescription = "a pokemon"
-                )
-                Text(
-                    text = pokemon.name,
-                    color = MaterialTheme.colors.onPrimary,
-                    style = MaterialTheme.typography.h5
-                )
 
-                Row(modifier= Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                    Text(modifier = Modifier.align(Alignment.CenterVertically).weight(1F),
-                        text = "N.º ${pokemon.number}",
-                        color = MaterialTheme.colors.onBackground,
-                        style = MaterialTheme.typography.caption
-                    )
+    @OptIn(ExperimentalFoundationApi::class)
+
+    data class RenderPokemonList(val pokemonList: List<Pokemon>) : Screen {
+
+        @Composable
+        override fun Content() {
+
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxWidth(), cells = GridCells.Fixed(2),
+                contentPadding = PaddingValues(10.dp)
+            ) {
+                items(pokemonList) { pokemon -> PokemonCard(pokemon = pokemon) }
+            }
+        }
+
+        @Composable
+        fun PokemonCard(pokemon: Pokemon) {
+            val navigator = LocalNavigator.currentOrThrow
+            Card(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clickable { navigator.push(PokemonDetails(pokemon.name)) }, shape = MaterialTheme.shapes.medium,
+                backgroundColor = pokemon.color
+            ) {
+                Column(
+                    Modifier.padding(horizontal = 10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Image(
-                        modifier = Modifier.padding(start = 25.dp).size(45.dp),
-                        painter = painterResource(id = pokemon.type),
-                        contentDescription = "pokemon type one"
+                        modifier = Modifier.size(100.dp),
+                        painter = painterResource(id = pokemon.image),
+                        contentDescription = "a pokemon"
                     )
+                    Text(
+                        text = pokemon.name,
+                        color = MaterialTheme.colors.onPrimary,
+                        style = MaterialTheme.typography.h5
+                    )
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .weight(1F),
+                            text = "N.º ${pokemon.number}",
+                            color = MaterialTheme.colors.onBackground,
+                            style = MaterialTheme.typography.caption
+                        )
+                        Image(
+                            modifier = Modifier
+                                .padding(start = 25.dp)
+                                .size(45.dp),
+                            painter = painterResource(id = pokemon.type),
+                            contentDescription = "pokemon type one"
+                        )
+                    }
                 }
             }
         }
     }
 
-    @OptIn(ExperimentalFoundationApi::class)
-    @Composable
-    fun PokemonList(pokemonList: List<Pokemon>) {
-        LazyVerticalGrid(
-            modifier = Modifier.fillMaxWidth(), cells = GridCells.Fixed(2),
-            contentPadding = PaddingValues(10.dp)
-        ) {
-            items(pokemonList) { pokemon -> PokemonCard(pokemon = pokemon) }
+    data class PokemonDetails(val pokemonName:String): Screen{
+        @Composable
+        override fun Content() {
+            Text(text = pokemonName)
         }
-    }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        Pokedex_composeTheme {
-            PokemonCard(
-                Pokemon(
-                    "Bulsabassaur",
-                    "001",
-                    GrassTypeGreen,
-                    R.drawable.ic_01,
-                    R.drawable.ic_grass
-                )
-            )
-        }
     }
 }
